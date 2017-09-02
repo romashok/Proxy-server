65ec9c3f217a1b7e3d458bd8872bd6e9c698fd60
Lebedev Roman <lebedeffroman@gmail.com>
2017-09-02 23:12:18 +0300

    Add request reading encapsulation to cliet class

--- proxy/client.cpp
+++ proxy/client.cpp
@@ -25,11 +25,37 @@ void client_t::set_state(client_state new_state) {
     state = new_state;
 }
 
+size_t client_t::read_request() {
+    read();
+
+    if (!has_request() && !create_new_request()) {
+        std::cout << "Allocation problems for new request!";
+        set_state(BAD_REQUEST); // should be?
+        return 0;
+    }
+
+    request->append_data(get_buffer());
+    buffer.clear();
+}
+
+bool client_t::is_bad_request() const noexcept {
+    return state == BAD_REQUEST;
+}
+
+bool client_t::is_ready() const noexcept {
+    return state == COMPLETED;
+}
+
+
+bool client_t::has_right_server() const noexcept {
+    return server && server->get_host() == request->get_host();
+}
+
 bool client_t::has_request() const noexcept {
     return request.get() != nullptr;
 }
 
-bool client_t::create_request() noexcept {
+bool client_t::create_new_request() noexcept {
     std::cout << "create new request" << std::endl;
     http_request* new_request = new (std::nothrow) http_request(get_fd());
     if (!new_request) return false;
@@ -47,8 +73,13 @@ void client_t::bind(struct server_t* new_server) {
     server->bind(this);
 }
 
+// can be safty used for client without the server
+void client_t::unbind() {
+    // todo better clearing
+    server.reset(nullptr);
+}
+
 bool client_t::has_server() const noexcept {
-    // todo unique_ptr op::bool
     return server.get() != nullptr;
 }
 
@@ -61,10 +92,9 @@ std::string client_t::get_request_host() const noexcept {
     }
 }
 
-void client_t::send_msg_to_server() {
+void client_t::move_request_to_server() {
     if (has_server()) {
-        server->append_to_buffer(buffer);
-        buffer.clear();
+        server->set_request(request.release());
     } else {
         std::cout << "Client error: No server to write!" << std::endl;
     }
@@ -77,4 +107,8 @@ int client_t::get_server_fd() {
         std::cout << "Client error: Get server fd error, no server." << std::endl;
         throw new custom_exception("No server");
     }
+}
+
+server_t* client_t::get_server() const noexcept {
+    return server.get();
 }
--- proxy/client.h
+++ proxy/client.h
@@ -21,20 +21,27 @@ struct client_t : public peer_t {
     client_state get_state() const noexcept;
     void set_state(client_state new_state);
 
-    bool has_request() const noexcept;
-    bool create_request() noexcept;
+    size_t read_request();
+    bool is_bad_request() const noexcept;
+    bool is_ready() const noexcept;
+    bool has_right_server() const noexcept;
+
     http_request* get_request() const noexcept;
 
     void bind(server_t* server);
+    void unbind();
     bool has_server() const noexcept;
 
     std::string get_request_host() const noexcept;
 
     int get_server_fd();
-    void send_msg_to_server();
-
+    server_t* get_server() const noexcept;
+    void move_request_to_server();
 private:
     client_state state;
     std::unique_ptr<struct server_t> server;
     std::unique_ptr<http_request> request;
+
+    bool has_request() const noexcept;
+    bool create_new_request() noexcept;
 };
