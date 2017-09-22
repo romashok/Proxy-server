9753644a5c8229056f33458bb8c6dba831f8f1b7
Lebedev Roman <lebedeffroman@gmail.com>
2017-09-22 22:09:20 +0300

    Add server interaction with http response class

--- proxy/server.cpp
+++ proxy/server.cpp
@@ -10,17 +10,21 @@
 #include "client.h"
 #include "server.h"
 #include "socket_api.h"
+#include "http/http_response_plain.h"
+#include "http/http_response_chunked.h"
 
 server_t::server_t(int fd):
         peer_t(fd),
         client(nullptr),
         request(nullptr),
+        response(nullptr)
 {}
 
 server_t::server_t(sockaddr addr):
         peer_t(socket_api::create_server_socket(addr)),
         client(nullptr),
         request(nullptr),
+        response(nullptr)
 {}
 
 void server_t::bind(client_t* new_client) {
@@ -29,6 +33,10 @@ void server_t::bind(client_t* new_client) {
 
 bool server_t::is_sent_all_request() const noexcept {
     return offset == request->get_raw_text().size();
+}
+
+http_response const* server_t::get_response() {
+    return response.get();
 }
 
 void server_t::set_request(http_request* new_request) {
@@ -58,4 +66,33 @@ std::string server_t::get_next_part() const {
     return request->get_raw_text().substr(offset);
 }
 
+void server_t::read_response() {
+    read();
+
+    if (!response) {
+        size_t i = buffer.find("Content-Length: ");
+        if (i != std::string::npos) {
+            std::cout << "new plain response" << std::endl;
+            response.reset(new http_response_plain());
+        }
+
+        i = buffer.find("Transfer-Encoding: chunked");
+        if (i != std::string::npos) {
+            std::cout << "new chunked response" << std::endl;
+            response.reset(new http_response_chunked());
+        }
+    }
+
+    if (response) {
+//        std::cout << "read response" << std::endl;
+//        std::cout << get_buffer().substr(0, 205) << std::endl;
+        response->append_data(get_buffer());
+        buffer.clear();
+//        std::exit(0);
+    }
+
+}
+
+void server_t::move_response_offset(size_t delta) {
+    response->move_offset(delta);
 }
--- proxy/server.h
+++ proxy/server.h
@@ -3,6 +3,7 @@
 
 #include "client.h"
 #include "http/http_request.h"
+#include "http/http_response.h"
 
 struct http_request;
 
@@ -13,17 +14,22 @@ struct server_t : peer_t {
     void bind(struct client_t* client);
     void set_request(http_request* new_request);
     bool is_sent_all_request() const noexcept;
+    http_response const* get_response();
 
     void set_host(std::string const& host);
     std::string get_host() const noexcept;
 
     int get_client_fd();
     void write_request();
+    void read_response();
+
+    void move_response_offset(size_t delta);
 private:
     std::string host;
     struct client_t* client;
     std::unique_ptr<http_request> request;
     size_t offset;
+    std::unique_ptr<http_response> response;
 
     std::string get_next_part() const;
 };
